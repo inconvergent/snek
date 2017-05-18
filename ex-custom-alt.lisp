6670968dff3437cdbe551fc487a7b03b56a90a04
Anders Hoff <inconvergent@gmail.com>
2017-05-18 20:13:08 +0200

    improvement to example

--- ex-custom-alt.lisp
+++ ex-custom-alt.lisp
@@ -16,15 +16,16 @@
         (i 0))
 
     (defun do-append-edge-alt* (snk a)
-      (aif (do-append-edge-alt snk a)
-           (progn
-             (setf curr it)
-             (incf i)
-             (sandpaint:set-rgba sand (list 0.0 0.7 0.7 0.01))
-             (sandpaint:circ sand (list (append-edge-alt-xy a)) 4 3000)
-             (sandpaint:set-rgba sand (list 0.0 0.0 0.0 0.01))
-             (circ-stroke sand (get-verts snk (list it (append-edge-alt-v a))))
-             (sandpaint:save sand (format nil "~a-~3,'0d" fn i)))))
+      (if (<= (length (verts-in-rad snk (append-edge-alt-xy a) rad)) 1)
+        (aif (do-append-edge-alt snk a)
+          (progn
+            (incf i)
+            (setf curr it)
+            (sandpaint:set-rgba sand (list 0.0 0.7 0.7 0.01))
+            (sandpaint:circ sand (list (append-edge-alt-xy a)) 4 3000)
+            (sandpaint:set-rgba sand (list 0.0 0.0 0.0 0.01))
+            (circ-stroke sand (get-verts snk (list it (append-edge-alt-v a))))
+            (sandpaint:save sand (format nil "~a-~3,'0d" fn i))))))
 
     (let ((snk (make-snek
                  :max-verts n
@@ -36,10 +37,10 @@
 
       (loop for i from 0 below n do
         (with-snek (snk :zwidth rad)
-          (let ((p (add (get-vert snk curr)
-                        (rnd-in-circ rad))))
-            (if (<= (length (verts-in-rad snk p rad)) 1)
-              (append-edge? curr p :rel nil))))))))
+          (append-edge?
+            curr (add (get-vert snk curr)
+                      (rnd-in-circ rad))
+            :rel nil))))))
 
 
 (defun main (size fn)
