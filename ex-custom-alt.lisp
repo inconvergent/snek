6f9927d5bcdf8ba3cc4a723eb0b80b803e4e2139
Anders Hoff <inconvergent@gmail.com>
2017-05-20 15:53:52 +0200

    use labels, not nested defuns

--- ex-custom-alt.lisp
+++ ex-custom-alt.lisp
@@ -15,32 +15,32 @@
   (let ((curr nil)
         (i 0))
 
-    (defun do-append-edge-alt* (snk a)
-      (if (<= (length (verts-in-rad snk (append-edge-alt-xy a) rad)) 1)
-        (aif (do-append-edge-alt snk a)
-          (progn
-            (incf i)
-            (setf curr it)
-            (sandpaint:set-rgba sand (list 0.0 0.7 0.7 0.01))
-            (sandpaint:circ sand (list (append-edge-alt-xy a)) 4 3000)
-            (sandpaint:set-rgba sand (list 0.0 0.0 0.0 0.01))
-            (circ-stroke sand (get-verts snk (list it (append-edge-alt-v a))))
-            (sandpaint:save sand (format nil "~a-~3,'0d" fn i))))))
+    (labels ((do-append-edge-alt* (snk a)
+               (if (<= (length (verts-in-rad snk (append-edge-alt-xy a) rad)) 1)
+                 (aif (do-append-edge-alt snk a)
+                   (progn
+                     (incf i)
+                     (setf curr it)
+                     (sandpaint:set-rgba sand (list 0.0 0.7 0.7 0.01))
+                     (sandpaint:circ sand (list (append-edge-alt-xy a)) 4 3000)
+                     (sandpaint:set-rgba sand (list 0.0 0.0 0.0 0.01))
+                     (circ-stroke sand (get-verts snk (list it (append-edge-alt-v a))))
+                     (sandpaint:save sand (format nil "~a-~3,'0d" fn i)))))))
 
-    (let ((snk (make-snek
-                 :max-verts n
-                 :max-grp-edges 0
-                 :max-main-grp-edges (* 4 n)
-                 :alts '((append-edge-alt do-append-edge-alt*)))))
+      (let ((snk (make-snek
+                   :max-verts n
+                   :max-grp-edges 0
+                   :max-main-grp-edges (* 4 n)
+                   :alts `((append-edge-alt ,#'do-append-edge-alt*)))))
 
-      (setf curr (add-vert! snk mid))
+        (setf curr (add-vert! snk mid))
 
-      (loop for i from 0 below n do
-        (with-snek (snk :zwidth rad)
-          (append-edge?
-            curr (add (get-vert snk curr)
-                      (rnd-in-circ rad))
-            :rel nil))))))
+        (loop for i from 0 below n do
+          (with-snek (snk :zwidth rad)
+            (append-edge?
+              curr (add (get-vert snk curr)
+                        (rnd-in-circ rad))
+              :rel nil)))))))
 
 
 (defun main (size fn)
