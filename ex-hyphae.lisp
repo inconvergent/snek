aab312242c9f871c8661f795b2151998de291929
Anders Hoff <inconvergent@gmail.com>
2017-05-20 02:49:57 +0200

    use labels

--- ex-hyphae.lisp
+++ ex-hyphae.lisp
@@ -10,60 +10,60 @@
   (let ((curr (make-hash-table :test #'equal))
         (hits 0))
 
-    (defun init (snk n)
-      (loop for i from 0 below n do
-        (setf (gethash
-                (add-vert! snk (rnd-in-box 250 250 :xy '(250 250)))
-                curr)
-              0)))
+    (labels ((init (snk n)
+               (loop for i from 0 below n do
+                 (setf (gethash
+                         (add-vert! snk (rnd-in-box 250 250 :xy '(250 250)))
+                         curr)
+                       0)))
 
-    (defun circ-stroke (vv)
-      (sandpaint:circ sand
-        (lin-path:pos* (lin-path:make vv) (linspace 0 0.99 100))
-        1 20))
+             (circ-stroke (vv)
+               (sandpaint:circ sand
+                 (lin-path:pos* (lin-path:make vv) (linspace 0 0.99 100))
+                 1 20))
 
-    (defun draw (snk a w)
-      (sandpaint:set-rgba sand (list 0.0 0.7 0.7 0.01))
-      (sandpaint:circ sand (list (append-edge-alt-xy a)) 4 3000)
-      (sandpaint:set-rgba sand (list 0.0 0.0 0.0 0.01))
-      (circ-stroke (get-verts snk (list w (append-edge-alt-v a)))))
+             (draw (snk a w)
+               (sandpaint:set-rgba sand (list 0.0 0.7 0.7 0.01))
+               (sandpaint:circ sand (list (append-edge-alt-xy a)) 4 3000)
+               (sandpaint:set-rgba sand (list 0.0 0.0 0.0 0.01))
+               (circ-stroke (get-verts snk (list w (append-edge-alt-v a)))))
 
-    (defun count-control (v)
-      (multiple-value-bind (c exists)
-        (gethash v curr)
-        (if exists
-          (if (> c 20)
-            (print (remhash v curr))
-            (incf (gethash v curr))))))
+             (count-control (v)
+               (multiple-value-bind (c exists)
+                 (gethash v curr)
+                 (if exists
+                   (if (> c 20)
+                     (print (remhash v curr))
+                     (incf (gethash v curr))))))
 
-    (defun do-append-edge-alt* (snk a)
-      (let ((v (append-edge-alt-v a)))
-        (count-control v)
-        (inside (size (append-edge-alt-xy a))
-          (if (<= (length (verts-in-rad snk (append-edge-alt-xy a) rad)) 1)
-            (aif (do-append-edge-alt snk a)
-              (progn
-                (incf hits)
-                (setf (gethash it curr) 0)
-                (draw snk a it)))))))
+             (do-append-edge-alt* (snk a)
+               (let ((v (append-edge-alt-v a)))
+                 (count-control v)
+                 (inside (size (append-edge-alt-xy a))
+                   (if (<= (length (verts-in-rad snk (append-edge-alt-xy a) rad)) 1)
+                     (aif (do-append-edge-alt snk a)
+                       (progn
+                         (incf hits)
+                         (setf (gethash it curr) 0)
+                         (draw snk a it))))))))
 
-    (let ((snk (make-snek
-                 :max-verts itt
-                 :max-grp-edges 0
-                 :max-main-grp-edges (* 4 itt)
-                 :alts '((append-edge-alt do-append-edge-alt*)))))
+      (let ((snk (make-snek
+                   :max-verts itt
+                   :max-grp-edges 0
+                   :max-main-grp-edges (* 4 itt)
+                   :alts `((append-edge-alt ,#'do-append-edge-alt*)))))
 
-      (init snk 20)
+        (init snk 20)
 
-      (loop for i from 0 below itt do
-        (with-snek (snk :zwidth rad)
-          (loop for k being the hash-keys in curr
-              collect
-            (append-edge? k
-              (add (get-vert snk k)
-                   (rnd-in-circ rad))
-              :rel nil))
-          (sandpaint:save sand (format nil "~a-~6,'0d" fn hits)))))))
+        (loop for i from 0 below itt do
+          (with-snek (snk :zwidth rad)
+            (loop for k being the hash-keys in curr
+                collect
+              (append-edge? k
+                (add (get-vert snk k)
+                     (rnd-in-circ rad))
+                :rel nil))
+            (sandpaint:save sand (format nil "~a-~6,'0d" fn hits))))))))
 
 
 (defun main (size fn)
